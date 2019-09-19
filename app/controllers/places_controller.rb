1efc3bf01364b4e924506795f6b82f7d6157203a
Your Name <you@example.com>
2019-09-19 19:19:34 +0000

    Lesson 20

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,6 @@
 class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @places = Place.all
   end
@@ -11,6 +12,10 @@ class PlacesController < ApplicationController
   def create
     current_user.places.create(place_params)
     redirect_to root_path
+  end
+
+  def show
+    @place = Place.find(params[:id])
   end
 
   private

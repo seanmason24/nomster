5fce86f2f24ef225a39ad360165b047588cc3e1f
Your Name <you@example.com>
2019-09-28 00:23:56 +0000

    29

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: https://nomster-sean-mason.herokuapp.com } 
+  config.action_mailer.default_url_options = { host: 'https://nomster-sean-mason.herokuapp.com' } 
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.

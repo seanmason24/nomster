b801731afc2859710c1f59d2390c0104709e10e1
Your Name <you@example.com>
2019-12-15 20:54:32 +0000

    Geocode fix

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :place
-  after_create :send_comment_email
 
   RATINGS = {
     'one star': '1_star',
@@ -10,10 +9,11 @@ class Comment < ApplicationRecord
     'four stars': '4_stars',
     'five stars': '5_stars'
   }
+
   def humanized_rating
     RATINGS.invert[self.rating]
   end
-  
+
   def send_comment_email
       NotificationMailer.comment_added(self).deliver_now
   end
